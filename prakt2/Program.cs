27443752c521fe3c594614338dabde523d7c9ef5
kchtkv-v <lerchik.kchtkv@gmail.com>
2022-10-14 08:44:26 +0300

    Оптимизация кода

--- prakt2/Program.cs
+++ prakt2/Program.cs
@@ -1,119 +1,63 @@
 ﻿using System;
+using System.Text;
 
 namespace prakt2
 {
-    class Program
+    public class Program
     {
-        private static string _str = "abcdefghiklmnopqrstvxyzABCDEFGHIKLMNOPQRSTVXYZ";
+        private const string _str = "abcdefghiklmnopqrstvxyzABCDEFGHIKLMNOPQRSTVXYZ";
         static void Main(string[] args)
         {
-            int number;
-            Console.WriteLine("Введите размер массивов");
-            try
+            Console.Write("Введите размер массивов: ");
+
+            int.TryParse(Console.ReadLine(), out int number);
+            if (number < 2 || number > 100)
             {
-                number = Convert.ToInt32(Console.ReadLine());
-                if (number > 1 && number <= 100)
-                {
-                    Console.Write("Первый массив: ");
-                    FillMas(number, out string[] mas1);
-                    Console.Write("Второй массив: ");
-                    FillMas(number, out string[] mas2);
-                    Console.Write("Третий массив: ");
-                    FillMas(number, out string[] mas3);
-                    Console.Write("Четвертый массив: ");
-                    FillMas(number, out string[] mas4);
-                    Console.Write("Пятый массив: ");
-                    FillMas(number, out string[] mas5);
-                    Console.WriteLine("Массивы с чередующимися строчными и прописными буквами:");
-                    Calculation(mas1, out bool result1);
-                    if (result1)
-                    {
-                        Console.Write("1 ");
-                    }
-                    Calculation(mas2, out bool result2);
-                    if (result2)
-                    {
-                        Console.Write("2 ");
-                    }
-                    Calculation(mas3, out bool result3);
-                    if (result3)
-                    {
-                        Console.Write("3 ");
-                    }
-                    Calculation(mas4, out bool result4);
-                    if (result4)
-                    {
-                        Console.Write("4 ");
-                    }
-                    Calculation(mas5, out bool result5);
-                    if (result5)
-                    {
-                        Console.Write("5");
-                    }
-                    if (result1 == false && result2 == false && result3 == false && result4 == false && result5 == false)
-                    {
-                        Console.Write("Таких массивов нет");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Размер массива должен быть больше одного и не больше ста");
-                }
+                Console.WriteLine("Размер массива должен быть больше одного и не больше ста");
+                return;
             }
-            catch
+
+            StringBuilder builder = new();
+            for (int i = 1; i < 6; i++)
             {
-                Console.WriteLine("Размер введен неверно");
+                Console.Write($"массив {i}: ");
+                var mas = FillMas(number);
+                if (Calculation(mas)) builder.Append(i + " ");
             }
+
+            if (builder.Length == 0)
+            {
+                Console.Write("Таких массивов нет");
+                return;
+            }
+            Console.WriteLine(builder);
             Console.ReadLine();
 
         }
-        private static void FillMas(int number, out string[] mas)
+        private static string[] FillMas(int number)
         {
-            mas = new string[number];
+            string[] mas = new string[number];
             Random rnd = new Random();
             for (int i = 0; i < number; i++)
             {
-                mas[i] = Convert.ToString(_str[rnd.Next(46)]);
+                mas[i] = Convert.ToString(_str[rnd.Next(_str.Length)]);
                 Console.Write(mas[i]);
             }
             Console.WriteLine();
+            return mas;
         }
-        private static void Calculation(string[] mas, out bool result)
+
+        private static bool Calculation(string[] mas)
         {
-            bool lower;
-            result = true;
-            if (mas[0] == mas[0].ToLower())
-            {
-                lower = true;
-            }
-            else lower = false;
+            bool first = mas[0] == mas[0].ToLower();
             for (int i = 1; i < mas.Length; i++)
             {
-                if (lower == true)
-                {
-                    if (mas[i] == mas[i].ToUpper())
-                    {
-                        lower = false;
-                    }
-                    else
-                    {
-                        result = false;
-                        break;
-                    }
-                }
-                else
-                {
-                    if (mas[i] == mas[i].ToLower())
-                    {
-                        lower = true;
-                    }
-                    else
-                    {
-                        result = false;
-                        break;
-                    }
-                }
+                bool actual = mas[i] == mas[i].ToLower();
+                if (actual == first) return false;
+                first = actual;
             }
+            return true;
         }
+
     }
 }
